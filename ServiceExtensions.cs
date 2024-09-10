33737167bff703d43a9a7028bf351b120d8d5e9b
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2024-09-10 18:25:40 +0200

    feat: add service extensions

---
+++ ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace OptimizelyDeleteMissingCommerceProperties;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddDeleteMissingCommerceProperties(this IServiceCollection services)
+    {
+        services.AddTransient<IMissingCommercePropertiesService, MissingCommercePropertiesService>();
+        services.AddTransient<DeleteMissingCommercePropertiesScheduledJob>();
+        services.AddTransient<ListMissingCommercePropertiesScheduledJob>();
+
+        return services;
+    }
+}
