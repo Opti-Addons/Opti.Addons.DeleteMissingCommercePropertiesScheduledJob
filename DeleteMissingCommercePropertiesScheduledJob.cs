6d1b89461cf9494c649bdd96c9cf539d71c6d3d6
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2024-09-10 18:24:37 +0200

    feat: update name, slightly adjust displayed text

--- DeleteMissingCommercePropertiesScheduledJob.cs
+++ DeleteMissingCommercePropertiesScheduledJob.cs
@@ -1,6 +1,6 @@
 ﻿namespace OptimizelyDeleteMissingCommerceProperties;
 
-[ScheduledPlugIn(DisplayName = "[Opti Addons] Delete missing Commerce data: Delete", 
+[ScheduledPlugIn(DisplayName = "[Opti.Addons] Delete missing Commerce data: Delete", 
     Description = "Removes properties that are not present in the model class(es). ⚠️ Warning: Please make sure to backup your database before running this job. " +
                   "You may also run the 'list' job first to see what properties will be removed.",
     GUID = "5DDFBF06-5C95-4EB7-964C-72D44133E496")]
@@ -25,7 +25,7 @@ public class DeleteMissingCommercePropertiesScheduledJob : ScheduledJobBase
 
         var sb = new StringBuilder();
         sb.AppendLine("Properties removed: (type.name)");
-        listPropertiesToBeRemoved.ForEach(p => sb.Append($"{p.Type.Name}.{p.Name()};<br>"));
+        listPropertiesToBeRemoved.ForEach(p => sb.Append($"{p.Type.Name}.{p.Name()}<br>"));
         
         return sb.ToString();
     }
